42a6e47913c2382c4c89775d4214e566504e6b9a
lerro-lerro <leonardocosta330@gmail.com>
2025-04-08 00:49:53 +0300

    part 3

--- part3/phonebook/eslint.config.mjs
+++ part3/phonebook/eslint.config.mjs
@@ -1,24 +1,26 @@
-import globals from 'globals'
-import js from '@eslint/js'
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default [
+  {
+    ignores: ["build/**"]
+  },
   js.configs.recommended,
   {
-    files: '**/*.js',
+    files: ["**/*.js"],
     languageOptions: {
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       globals: {
         ...globals.node,
       },
-      ecmaVersion: 'latest',
+      ecmaVersion: "latest",
     },
     rules: {
-      eqeqeq: 'error',
-      'no-trailing-spaces': 'error',
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { before: true, after: true }],
-      'no-console': 'off'
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": "off"
     },
-    ignores: ['dist/**'],
   }
-]
+];
